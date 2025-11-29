6030bb2cb3ee93c996ffed05f78daf5a3f73cfac
UlvenRev <bulinchik@gmail.com>
2025-11-29 11:21:14 +0000

    In-between station movement animation
    Clicking on the marker gets you:
    1. Centering over the starting point
    2. Zooming in
    3. Movement to the target station
    4. Zooming out once reached the destination

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,6 +2,8 @@ import { useRef, useEffect, useState } from 'react'
 import mapboxgl from 'mapbox-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
 
+import marker_logo from '../assets/marker.svg'
+
 export default function Map() {
     const mapRef = useRef();
     const mapContainerRef = useRef();
@@ -17,7 +19,7 @@ export default function Map() {
             container: mapContainerRef.current,
             style: "mapbox://styles/ulvenrev/cmiisp64o00na01qtg8i24fpe",
             center: [userStartingPoint.lng, userStartingPoint.lat],
-            zoom: 6.3,
+            zoom: 7,
             minZoom: 5,
             maxZoom: 7
         });
@@ -33,11 +35,77 @@ export default function Map() {
         markers.forEach((marker) => {
             let markerClass = marker.className.split(' ')[marker.className.split(' ').length - 1];
             
-            marker.onmouseenter = () => {  // When we hover over the marker instead of clicking it
-                console.log(markerClass);  // it prints the name of the marker, i.e. the last class we added manually above
+            // CUSTOM MARKER SVG
+            marker.innerHTML = "<img>";
+            marker.children[0].src = marker_logo;
+            
+            // HOVERING 
+            marker.onmouseenter = () => { 
+                // Do something here that will pass the NAME of the station to the button below 
+                // If clicked - then proceed to the confirmation window and then to movement
+                // But when simply hovered over, we pass the info to the button and that's it
+                // The expansion and glowing is done in css
             }
 
-        })
+            // MOVING TO THE STATION
+            marker.onclick = () => {
+                mapRef.current.flyTo({
+                    center: [8.191225, 46.015261],  // FLying back to the USER POINT and starting from there
+                    zoom: 7,
+                    speed: 0.2,
+                    curve: 1,
+                    easing(t) {
+                        return t;
+                    }
+                });
+
+                mapRef.current.once('moveend', () => {  // .once('moveend') is used to track when the PREV animation ENDED - then we start a new one, otherwise it will be jumpy
+                    // Setting zoom and changing the style to the colorful one
+                    mapRef.current.setMaxZoom(9);
+                    mapRef.current.zoomTo(9, {
+                        duration: 2000
+                    });
+                    // mapRef.current.setStyle("mapbox://styles/ulvenrev/cmik0ioyv003001sbcqbl2wi9");
+
+                    // Moving
+                    mapRef.current.once('moveend', () => {
+                        console.log("ZOOMED");
+                        if (markerClass == "userMarker")
+                        {
+                            mapRef.current.flyTo({
+                                center: [8.191225, 46.015261],
+                                zoom: 9,
+                                speed: 0.2,
+                                curve: 1,
+                                easing(t) {
+                                    return t;
+                                }
+                            });
+                        } else {
+                            mapRef.current.flyTo({
+                                center: [8, 47],
+                                zoom: 9,
+                                speed: 0.2,
+                                curve: 1,
+                                easing(t) {
+                                    return t;
+                                }
+                            });
+                        }
+
+                        // Changing the zoom back to normal
+                        mapRef.current.once('moveend', () => {
+                            mapRef.current.zoomTo(7, {
+                                duration: 2000
+                            });
+                            mapRef.current.once('moveend', () => {     
+                                mapRef.current.setMaxZoom(7);
+                            });
+                        });
+                    });
+                });
+            };
+        });
         
         return () => {
             mapRef.current.remove();
