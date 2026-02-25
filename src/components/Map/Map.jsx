a328029b3c0847236df49a2d36a2f773ef2e9eb4
UlvenRev <bulinchik@gmail.com>
2026-02-25 21:04:34 +0000

    Pause Function Added

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -10,7 +10,7 @@ import user_marker_logo from '../../assets/images/black_user_marker.svg'
 import train_icon from '../../assets/images/train_icon.png'
 import exit from '../../assets/images/exit.svg'
 
-import { calcStationParameters } from '../../assets/utils/mapFunctions.js'
+import { calcStationParameters, calculateTravelTimeInMinutes } from '../../assets/utils/mapFunctions.js'
 
 import { updateCurrentStation, getCurrentStationId } from '../../api/users.js';
 
@@ -36,6 +36,9 @@ export default function Map() {
     const popupOpenRef = useRef(false);
 
     const currentlyTravelling = useRef(false);
+    const currentlyPaused = useRef(false);
+    const waitForEvent = (map, event) => new Promise(resolve => map.once(event, resolve));  // Helper function for animations - removes the nested loops by waiting for the previous event to finish and only then proceed to the next one
+
     const [timeAndCoords, setTimeAndCoords] = useState({hours: null, minutes: null, nextLng: null, nextLat: null, stationId: null});  // These need to be kept between renders => use useState for this
     const UI_elements_div = useRef(null);  // For the UI elements container to make it pointer-events: auto when the pop up is opened (so that we can't move the map)
 
@@ -86,8 +89,9 @@ export default function Map() {
             style: "mapbox://styles/ulven-rev/cml9m8wbr007001sj277u888r",
             center: [userStartingPoint.lng, userStartingPoint.lat],
             zoom: 7,
-            minZoom: 1,
-            maxZoom: 7
+            minZoom: 5,
+            maxZoom: 7,
+            pitchWithRotate: false  // Do disable the tilting ability
         });
 
         // Create markers once
@@ -196,86 +200,79 @@ export default function Map() {
     }
 
     async function animateMapMovement(nextLng, nextLat, travelTime, stationId) {
-        setPopupWindow(prev => !prev);
-        popupOpenRef.current = false;
-        UI_elements_div.current.style.pointerEvents = 'auto';
-        currentlyTravelling.current = true;
+        if (!currentlyPaused.current) {  // We weren't UNPAUSING and calling this animation, we JUST STARTED it so fly back to the user point
+            setPopupWindow(prev => !prev);
+            popupOpenRef.current = false;
+            UI_elements_div.current.style.pointerEvents = 'auto';
+            currentlyTravelling.current = true;
+            currentlyPaused.current = false;
 
-        // FLying back to the USER POINT and starting from there
-        mapRef.current.flyTo({
-            center: [userStartingPoint.lng, userStartingPoint.lat], 
-            zoom: 7,
-            speed: 0.5,
-            curve: 1,
-            easing(t) {
-                return t;
-            }
-        });
-
-        mapRef.current.once('moveend', () => {  // .once('moveend') is used to track when the PREV animation ENDED - then we start a new one, otherwise it will be jumpy
-            // Zooming in 
+            mapRef.current.flyTo({
+                center: [userStartingPoint.lng, userStartingPoint.lat], 
+                zoom: 7,
+                speed: 0.7,
+                curve: 1,
+                easing(t) {
+                    return t;
+                }
+            });
+            await waitForEvent(mapRef.current, 'moveend');
+            if (!currentlyTravelling.current || currentlyPaused.current) return;  // If NOT travelling or IS PAUSED - don't animate
 
             mapRef.current.setMaxZoom(11.8);
-            mapRef.current.zoomTo(11.8, {
-                duration: 5000
-            });
+            mapRef.current.zoomTo(11.8, { duration: 3200 });
+            await waitForEvent(mapRef.current, 'zoomend');
+            if (!currentlyTravelling.current || currentlyPaused.current) return;
+        } else {  // Otherwise, currentlyPaused was TRUE so change it to FALSE and start moving from the point we stopped on
+            currentlyPaused.current = false;
+        }
 
-            mapRef.current.once('zoomend', () => {  // Animation continues
-                mapRef.current.once('moveend', () => {
-                    train_icon_div.current.style.opacity = '1';
-
-                    mapRef.current.easeTo({
-                        center: [nextLng, nextLat],
-                        zoom: 11.8,
-                        duration: 60000*travelTime, // 1 minute = 60 000 ms and we set duration in ms
-                        // duration: 20000,
-                        easing: (t) => t  // Linear animation - no slowdown at the end
-                    });
-
-                    // Changing zoom back to normal
-                    mapRef.current.once('moveend', () => {
-                        train_icon_div.current.style.opacity = '0';
-
-                        mapRef.current.zoomTo(7, {
-                            duration: 5000
-                        });
-                        
-                        mapRef.current.once('moveend', async () => {    
-                            if (currentlyTravelling.current) {  // Check this in case the user used stopTravelling() function but the code still reached this part - don't update any info if we interrupted the journey 
-                                mapRef.current.setMaxZoom(7);
-                                setTimeAndCoords({hours: null, minutes: null, nextLng: null, nextLat: null, stationId: null});
-                                UI_elements_div.current.style.pointerEvents = 'none';
-                                currentlyTravelling.current = false;
-
-                                if (stationId) {
-                                    try {
-                                        // update current station
-                                        await updateCurrentStation(stationId);
-                                        console.log("Updated currentStationId: ", stationId);
-
-                                        // Update local state so app does not reset
-                                        const station = arrayOfStations.find(s => s.id === stationId);
-                                        if (station) {
-                                            setUserStartingPoint({
-                                                lng: station.longitude,
-                                                lat: station.latitude,
-                                                name: station.name
-                                            });
-                                        }
-
-                                    } catch (err) {
-                                        console.error("Failed to update currentStation:", err);
-                                    }
-                                }
-                            } 
-                        });
-                    });
-                });
-            });
+        train_icon_div.current.style.opacity = '1';
+        mapRef.current.easeTo({
+            center: [nextLng, nextLat],
+            zoom: 11.8,
+            duration: 60000*travelTime, // 1 minute = 60 000 ms and we set duration in ms
+            // duration: 20000,
+            easing: (t) => t  // Linear animation - no slowdown at the end
         });
+        await waitForEvent(mapRef.current, 'moveend');
+        if (!currentlyTravelling.current || currentlyPaused.current) return;
+
+        train_icon_div.current.style.opacity = '0';
+        mapRef.current.zoomTo(7, { duration: 3200 });
+        await waitForEvent(mapRef.current, 'zoomend');
+        if (!currentlyTravelling.current || currentlyPaused.current) return;
+
+        if (currentlyTravelling.current || !currentlyPaused.current) {  // Check this in case the user used stopTravelling() function but the code still reached this part - don't update any info if we interrupted the journey 
+            mapRef.current.setMaxZoom(7);
+            setTimeAndCoords({hours: null, minutes: null, nextLng: null, nextLat: null, stationId: null});
+            UI_elements_div.current.style.pointerEvents = 'none';
+            currentlyTravelling.current = false;
+
+            if (stationId) {
+                try {
+                    // update current station
+                    await updateCurrentStation(stationId);
+                    console.log("Updated currentStationId: ", stationId);
+
+                    // Update local state so app does not reset
+                    const station = arrayOfStations.find(s => s.id === stationId);
+                    if (station) {
+                        setUserStartingPoint({
+                            lng: station.longitude,
+                            lat: station.latitude,
+                            name: station.name
+                        });
+                    }
+
+                } catch (err) {
+                    console.error("Failed to update currentStation:", err);
+                }
+            }
+        } 
     }
 
-    function stopTravelling() {
+    async function stopTravelling() {
         setPopupWindow(prev => !prev);  // This re-render is triggered ONLY after the previous line
         popupOpenRef.current = false;
         currentlyTravelling.current = false;
@@ -283,19 +280,30 @@ export default function Map() {
         mapRef.current.zoomTo(7, {  // Zooming back out
             duration: 5000
         });
-        mapRef.current.once('moveend', () => {
-            mapRef.current.setMaxZoom(7);
-            train_icon_div.current.style.opacity = '0';
-            UI_elements_div.current.style.pointerEvents = 'none';
-            setTimeAndCoords({hours: null, minutes: null, nextLng: null, nextLat: null, stationId: null});  // Nothing new for the new coordinates, they stay empty until the user chooses the next destination
-            setNextStation({name: 'at station', country: ''});
-            setTravelTimeLabel('Awaiting travelling...');
-        });
+        await waitForEvent(mapRef.current, 'moveend');
+
+        mapRef.current.setMaxZoom(7);
+        train_icon_div.current.style.opacity = '0';
+        UI_elements_div.current.style.pointerEvents = 'none';
+        setTimeAndCoords({hours: null, minutes: null, nextLng: null, nextLat: null, stationId: null});  // Nothing new for the new coordinates, they stay empty until the user chooses the next destination
+        setNextStation({name: 'at station', country: ''});
+        setTravelTimeLabel('Awaiting travelling...');
     }
 
-    function pauseTravelling() {
-        mapRef.current.stop();
-        console.log("PAUSED");
+    function togglePauseState() {
+        const pausePosition = {lng: mapRef.current.getCenter()[0], lat: mapRef.current.getCenter()[1]};
+
+        // Calculate travel time left in minutes based on the CURRENT CENTER OF THE MAP (pausePosition) and the final coordinates
+        const timeLeft = calculateTravelTimeInMinutes(mapRef.current.getCenter(), timeAndCoords.nextLng, timeAndCoords.nextLat);
+
+        if (!currentlyPaused.current) {  // We clicked to PAUSE the animation
+            mapRef.current.stop();
+            currentlyPaused.current = true;
+            setTravelTimeLabel('Session paused');
+        } else {  // If currentlyPaused is true, we clicked to UN-PAUSE the animation
+            setTravelTimeLabel('Travelling...');  // Added only for re-rendering the code => updating the bottom UI 
+            animateMapMovement(timeAndCoords.nextLng, timeAndCoords.nextLat, timeLeft, timeAndCoords.stationId);
+        }
     }
     
     return (
@@ -331,8 +339,9 @@ export default function Map() {
             <BottomUI 
                 travelTimeLabel = {travelTimeLabel}
                 currentlyTravelling = {currentlyTravelling}
+                currentlyPaused = {currentlyPaused}
                 nextStation = {nextStation}
-                pauseTravelling = {pauseTravelling}
+                togglePauseState = {togglePauseState}
                 timerDuration = {timeAndCoords.hours*60+timeAndCoords.minutes}
             />
         </div>
