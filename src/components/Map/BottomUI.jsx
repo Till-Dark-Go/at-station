9db3a0ef630ad94ada1e4b639bb91bed40caa8e6
Vitalii <vitalii.bodnia@gmail.com>
2026-03-13 10:36:25 +0000

    Merge fix

--- src/components/Map/BottomUI.jsx
+++ src/components/Map/BottomUI.jsx
@@ -5,30 +5,14 @@ import Timer from "./Timer.jsx";
 import { useState } from "react";
 
 export default function BottomUI(props) {
-    return (
-        <div className='bottom-UI'>
-            {(!props.currentlyTravelling.current || props.currentlyPaused.current) && <div className='travel-time-bar'>{props.travelTimeLabel}</div>}
-            {props.currentlyTravelling.current && !props.currentlyPaused.current && <div className='travel-time-bar'>
-                <Timer duration = {props.timerDuration}/>
-            </div>}
-            <div className='buttons'>
-                <button
-                    className='todo-list-button'
-                ><img src={todo_list_logo} alt="Todo list page logo" /></button>
-                {!props.currentlyTravelling.current && 
-                <button className='at-station-button'>
-                    {props.nextStation.name}
-                </button>}
-                {props.currentlyTravelling.current && 
-                <button className='at-station-button travelling'
-                    onClick = {props.togglePauseState}
-                >
-                    {props.currentlyPaused.current ? "resume" : "pause"}
-                </button>}
-                <button
-                    className='profile-button'
-                ><img src={user_pf_logo} alt="User profile page logo" /></button>
-            </div>
+  return (
+    <div className="bottom-UI">
+      {!props.currentlyTravelling.current && (
+        <div className="travel-time-bar">{props.travelTimeLabel}</div>
+      )}
+      {props.currentlyTravelling.current && (
+        <div className="travel-time-bar">
+          <Timer duration={props.timerDuration} />
         </div>
       )}
       <div className="buttons">
