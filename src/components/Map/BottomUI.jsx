927f9e4c10d764bc07ee3a5f3379afb0b95412da
UlvenRev <bulinchik@gmail.com>
2026-03-23 16:09:00 +0000

    All fixes from both merges

--- src/components/Map/BottomUI.jsx
+++ src/components/Map/BottomUI.jsx
@@ -5,57 +5,40 @@ import stamps_logo from '../../assets/images/stamps_logo.svg'
 import Timer from './Timer.jsx'
 
 export default function BottomUI(props) {
-    return (
-        <div className='bottom-UI'>
-            {(!props.currentlyTravelling.current || props.currentlyPaused.current) && <div className='travel-time-bar'>{props.travelTimeLabel}</div>}
-            {props.currentlyTravelling.current && !props.currentlyPaused.current && <div className='travel-time-bar'>
-                <Timer duration = {props.timerDuration}/>
-            </div>}
-            <div className='buttons'>
-                <button
-                    className='feature-button todo-list-button'
-                ><img src={todo_list_logo} alt="Todo list page logo" /></button>
-                <button
-                    className='feature-button stamps-button'
-                    onClick = {props.toggleStampsWindow}
-                ><img src={stamps_logo} alt="Stamps page logo" /></button>
-                {!props.currentlyTravelling.current && 
-                <button className='at-station-button'>
-                    {props.nextStation.name}
-                </button>}
-                {props.currentlyTravelling.current && 
-                <button className='at-station-button travelling'
-                    onClick = {props.togglePauseState}
-                >
-                    {props.currentlyPaused.current ? "resume" : "pause"}
-                </button>}
-                <button
-                    className='feature-button profile-button'
-                ><img src={user_pf_logo} alt="User profile page logo" /></button>
-            </div>
+  return (
+    <div className='bottom-UI'>
+        {(!props.currentlyTravelling.current || props.currentlyPaused.current) && <div className='travel-time-bar'>{props.travelTimeLabel}</div>}
+
+        {props.currentlyTravelling.current && !props.currentlyPaused.current && <div className='travel-time-bar'>
+            <Timer duration = {props.timerDuration}/>
+        </div>}
+
+        <div className='buttons'>
+            <button
+                className='feature-button todo-list-button' 
+                onClick={props.openTodoList}
+            ><img src={todo_list_logo} alt="Todo list page logo" /></button>
+
+            <button
+                className='feature-button stamps-button'
+                onClick = {props.toggleStampsWindow}
+            ><img src={stamps_logo} alt="Stamps page logo" /></button>
+
+            {!props.currentlyTravelling.current && 
+            <button className='at-station-button'>
+                {props.nextStation.name}
+            </button>}
+            {props.currentlyTravelling.current && 
+            <button className='at-station-button travelling'
+                onClick = {props.togglePauseState}
+            >
+                {props.currentlyPaused.current ? "resume" : "pause"}
+            </button>}
+            
+            <button
+                className='feature-button profile-button'
+            ><img src={user_pf_logo} alt="User profile page logo" /></button>
         </div>
-      )}
-      <div className="buttons">
-        <button className="todo-list-button" onClick={props.openTodoList}>
-          <img src={todo_list_logo} alt="Todo list page logo" />
-        </button>
-        {!props.currentlyTravelling.current && (
-          <button className="at-station-button">
-            {props.nextStation.name}
-          </button>
-        )}
-        {props.currentlyTravelling.current && (
-          <button
-            className="at-station-button travelling"
-            onClick={props.pauseTravelling}
-          >
-            pause
-          </button>
-        )}
-        <button className="profile-button">
-          <img src={user_pf_logo} alt="User profile page logo" />
-        </button>
-      </div>
     </div>
-  );
+  )
 }
